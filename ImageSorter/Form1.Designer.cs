f6887640044f1498c29d89acf47eda964d15eb1a
klowdo <klowdo.fs@gmail.com>
2015-05-02 13:12:57 +0200

    First Upload
    Beta! Need to be non thread blocking and user info

---
+++ ImageSorter/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ImageSorter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.FromPath = new System.Windows.Forms.Button();
+            this.ToPath = new System.Windows.Forms.Button();
+            this.SortBtn = new System.Windows.Forms.Button();
+            this.Recursive_Check = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // FromPath
+            // 
+            this.FromPath.Location = new System.Drawing.Point(70, 12);
+            this.FromPath.Name = "FromPath";
+            this.FromPath.Size = new System.Drawing.Size(75, 23);
+            this.FromPath.TabIndex = 0;
+            this.FromPath.Text = "From";
+            this.FromPath.UseVisualStyleBackColor = true;
+            this.FromPath.Click += new System.EventHandler(this.FromPath_Click);
+            // 
+            // ToPath
+            // 
+            this.ToPath.Location = new System.Drawing.Point(70, 59);
+            this.ToPath.Name = "ToPath";
+            this.ToPath.Size = new System.Drawing.Size(75, 23);
+            this.ToPath.TabIndex = 1;
+            this.ToPath.Text = "To";
+            this.ToPath.UseVisualStyleBackColor = true;
+            this.ToPath.Click += new System.EventHandler(this.ToPath_Click);
+            // 
+            // SortBtn
+            // 
+            this.SortBtn.Location = new System.Drawing.Point(70, 126);
+            this.SortBtn.Name = "SortBtn";
+            this.SortBtn.Size = new System.Drawing.Size(75, 23);
+            this.SortBtn.TabIndex = 2;
+            this.SortBtn.Text = "Sort";
+            this.SortBtn.UseVisualStyleBackColor = true;
+            this.SortBtn.Click += new System.EventHandler(this.SortBtn_Click);
+            // 
+            // Recursive_Check
+            // 
+            this.Recursive_Check.AutoSize = true;
+            this.Recursive_Check.Location = new System.Drawing.Point(65, 103);
+            this.Recursive_Check.Name = "Recursive_Check";
+            this.Recursive_Check.Size = new System.Drawing.Size(80, 17);
+            this.Recursive_Check.TabIndex = 3;
+            this.Recursive_Check.Text = "Recursive?";
+            this.Recursive_Check.UseVisualStyleBackColor = true;
+            this.Recursive_Check.CheckedChanged += new System.EventHandler(this.Recursive_Check_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(228, 212);
+            this.Controls.Add(this.Recursive_Check);
+            this.Controls.Add(this.SortBtn);
+            this.Controls.Add(this.ToPath);
+            this.Controls.Add(this.FromPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FromPath;
+        private System.Windows.Forms.Button ToPath;
+        private System.Windows.Forms.Button SortBtn;
+        private System.Windows.Forms.CheckBox Recursive_Check;
+    }
+}
+
---
+++ ImageSorter/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ImageSorter
+{
+    public partial class Form1 : Form
+    {
+        private string _FromPath;
+        private string _ToPath;
+        private bool _recursive = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FromPath_Click(object sender, EventArgs e)
+        {
+            _FromPath = GetPath();
+        }
+        /// <summary>
+        /// Just dialog to get users path
+        /// </summary>
+        /// <returns></returns>
+        private string GetPath()
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            DialogResult result = fbd.ShowDialog();
+            return fbd.SelectedPath;
+        }
+
+        private void ToPath_Click(object sender, EventArgs e)
+        {
+            _ToPath = GetPath();
+        }
+
+        private void SortBtn_Click(object sender, EventArgs e)
+        {
+            SortFiles(_FromPath);
+        }
+        /// <summary>
+        /// Sorting files in to _ToPath/YYYY-MM
+        /// </summary>
+        /// <param name="folder"></param>
+        private void SortFiles(string folder)
+        {
+            if (_recursive)
+            {
+                var directorys = Directory.GetDirectories(folder);
+                foreach (string directory in directorys)
+                {
+                    SortFiles(directory);
+                }
+            }
+            var files = GetImageFiles(folder);
+            foreach (var file in files)
+            {
+                DateTime fileDateTime;
+                Console.Out.WriteLine(file);
+                try
+                {
+                    fileDateTime = GetDateTakenFromImage(file);
+                }
+                catch (Exception)
+                {
+                    fileDateTime = File.GetCreationTime(file) > File.GetLastWriteTime(file)
+                        ? File.GetLastWriteTime(file)
+                        : File.GetCreationTime(file);
+                }
+                string path = string.Format("{0}\\{1:yyyy}\\{1:MMM}", _ToPath, fileDateTime);
+                string Newfilepath = string.Format("{0}\\{1}", path, Path.GetFileName(file));
+                string tempfilename = Newfilepath;
+                int NewIntFileName = 1;
+                while (File.Exists(tempfilename))
+                {
+                    tempfilename = string.Format("{0}({1}).{2}", Path.GetFileNameWithoutExtension(Newfilepath),NewIntFileName,Path.GetExtension(Newfilepath));
+                    NewIntFileName++;
+                }
+                Newfilepath = tempfilename;
+                
+                if (!Directory.Exists(path))
+                {
+                        Directory.CreateDirectory(path);
+                }
+                Console.Out.WriteLine("kopierar");
+                File.Copy(file, Newfilepath);
+                
+
+                Console.Out.WriteLine(File.GetCreationTime(file));
+            }
+        }
+        private static IEnumerable<string> GetImageFiles(string sourceFolder)
+        {
+            return from file in System.IO.Directory.EnumerateFiles(sourceFolder)
+                   let extension = Path.GetExtension(file)
+                   where extension == ".jpg" || extension == ".gif" || extension == ".png"
+                   select file;
+        }
+
+        private static Regex r = new Regex(":");
+
+        //retrieves the datetime WITHOUT loading the whole image
+        public static DateTime GetDateTakenFromImage(string path)
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (Image myImage = Image.FromStream(fs, false, false))
+            {
+                PropertyItem propItem = myImage.GetPropertyItem(36867);
+                string dateTaken = r.Replace(Encoding.UTF8.GetString(propItem.Value), "-", 2);
+                return DateTime.Parse(dateTaken);
+            }
+        }
+
+        
+        // Change recursive option
+        private void Recursive_Check_CheckedChanged(object sender, EventArgs e)
+        {
+            _recursive = Recursive_Check.Checked;
+        }
+    }
+}
